d45c4bd24064b2ccced8cf9fa7ffb8f56bcf07fd
tushandargan <tushandargan@gmail.com>
2024-01-26 13:40:13 -0500

    push

--- website/scripts/main.js
+++ website/scripts/main.js
@@ -1,6 +1,17 @@
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
 
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/fractal.gif") {
+    myImage.setAttribute("src", "images/koch_fractal.jpeg");
+  } else {
+    myImage.setAttribute("src", "images/fractal.gif");
+  }
+};
+
 function setUserName() {
     const myName = prompt("Please enter your name.");
     if (!myName) {
