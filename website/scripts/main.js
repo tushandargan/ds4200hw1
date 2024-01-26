1b3e595c1e8373276f6e7c64fd49757895ce2001
tushandargan <tushandargan@gmail.com>
2024-01-26 13:02:29 -0500

    change
    change

---
+++ website/scripts/main.js
@@ -0,0 +1,16 @@
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `${myName}`;
+    }
+  }
+
+myButton.onclick = () => {
+setUserName();
+};
